78589e36eed35bcbd70525de2b246026d9ce4951
shah <shahrokh.r1379@gmail.com>
2024-03-20 18:32:16 +0330

    fix user authentication in def listview

--- auction/views.py
+++ auction/views.py
@@ -13,20 +13,22 @@ from django.http import HttpResponseRedirect
 
 def listview(request):
     auctions = Auction.objects.all()
+    if request.user.is_authenticated:
+        user = User.objects.get(username=request.user.username)
 
-    user = User.objects.get(username=request.user.username)
 
-
-    w = Watchlist.objects.filter(user_id=user)
-    watchlist = Auction.objects.none()
-    for item in w:
-        a = Auction.objects.filter(id=item.auction_id.id)
-        watchlist = list(chain(watchlist, a))
-            
-    userDetails = UserDetails.objects.get(user_id=user.id)
-    return render(request, 'listview.html', {
-        'auctions': auctions, 'balance': userDetails.balance, 'watchlist': watchlist})
-
+        w = Watchlist.objects.filter(user_id=user)
+        watchlist = Auction.objects.none()
+        for item in w:
+            a = Auction.objects.filter(id=item.auction_id.id)
+            watchlist = list(chain(watchlist, a))
+                
+        userDetails = UserDetails.objects.get(user_id=user.id)
+        return render(request, 'listview.html', {
+            'auctions': auctions, 'balance': userDetails.balance, 'watchlist': watchlist})
+    else:
+        return render(request, 'listview.html', {'auctions': auctions})
+    
     
 def detailview(request, auction_id):
     if request.user.is_authenticated:
