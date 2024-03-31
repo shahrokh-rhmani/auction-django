0773d7745df94667bba48a17482040cab55ef20b
shah <shahrokh.r1379@gmail.com>
2024-03-31 07:19:28 +0330

    fix bug time_left

--- auction/templatetags/custom_tags.py
+++ auction/templatetags/custom_tags.py
@@ -9,9 +9,9 @@ register = template.Library()
 def time_left(value):
     t = value - timezone.now()
     days, seconds = t.days, t.seconds
-    hours = days * 24 + seconds // 3600
-    minutes = (seconds % 3600) // 60
-    seconds = seconds % 60
+    hours =  seconds // 3600 # 60**2 # 86400 // 3600 = 24
+    minutes = (seconds % 3600) // 60 # 12600 % 3600 = 1800 ==> 1800 // 60 = 30
+    seconds = seconds % 60 # seconds % (24 * 3600) % 60
     if value > timezone.now():
         return str(days) + "d " + str(hours) + "h " + str(minutes) + "m " + str(seconds) + "s"
     else:
